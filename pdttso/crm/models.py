eab0d00421e30d2772eadc4e878e2ef28fc7fd17
wasper256 <andremorhun@gmail.com>
2019-01-26 08:40:09 +0200

    Add models

--- pdttso/crm/models.py
+++ pdttso/crm/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+
+class Application(models.Model):
+    name = models.CharField(max_length=50)
+    agreement_id = models.IntegerField()
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
--- pdttso/erp/models.py
+++ pdttso/erp/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
-# Create your models here.
+
+class Payments(models.Model):
+    agreement_id = models.IntegerField()
+    amount = models.DecimalField(max_digits=30, decimal_places=2)
+    date = models.DateTimeField(auto_now_add=True)
